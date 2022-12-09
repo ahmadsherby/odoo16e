2f14c35be0a785322280b427a09e1fec0cd2bf4f
ahmedsalama51 <ahmed.salama1679@gmail.com>
2022-12-09 21:34:08 +0200

    [CHANGE] fix milestone of product_id domain on pol

--- stone_production/models/purchase_order_changes.py
+++ stone_production/models/purchase_order_changes.py
@@ -51,6 +51,28 @@ class PurchaseOrderLine(models.Model):
         for line in self:
             line.price_unit = line.product_id.item_total_cost
 
+    @api.onchange('item_type_id', 'order_id')
+    @api.depends('item_type_id', 'order_id.stone_production_product_ids')
+    def _get_product_domain(self):
+        """
+        Get Domain or products depend on value of New Products Field
+        :return:
+        """
+        domain = []
+        if self.order_id.company_id:
+            domain = ['|', ('company_id', '=', False), ('company_id', '=', self.order_id.company_id.id)]
+        if self.order_id.stone_production_product_ids:
+            domain.append(('id', 'in', self.order_id._origin.stone_production_product_ids.mapped('product_variant_id.id')))
+        else:
+            domain.append(('purchase_ok', '=', True))
+            domain.append(('item_type_id', '=', False))
+        print("Domain: ", domain)
+        product_ids = self.env['product.product'].search(domain).ids
+        print("product_ids: ", product_ids)
+        return {'domain': {'product_id': [('id', 'in', product_ids)]}}
+
+    product_id = fields.Many2one(domain=_get_product_domain)
+
     item_type_id = fields.Many2one(related='product_id.item_type_id')
     item_type_size_eq = fields.Selection(related='item_type_id.size')
     color_id = fields.Many2one(related='product_id.color_id')
